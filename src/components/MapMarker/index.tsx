49f0f7841ad396b0c05581bd53674469429de92c
Peter Mansell <peter.mansell.ext@lvs.co.uk>
2019-07-04 17:00:26 +0100

    Use relative url

--- src/components/MapMarker/index.tsx
+++ src/components/MapMarker/index.tsx
@@ -4,7 +4,7 @@ import { MapMarkerWrapper, MarkerImage } from './styles';
 
 export const MapMarker: SFC<MapMarkerModel> = ({ post : { photo, slug }, hovered }) => (
     <MapMarkerWrapper hovered={hovered}>
-        <MarkerImage to={`/posts/${slug}`} style={{backgroundImage: `url(/images/${photo})`}} />
+        <MarkerImage to={`/posts/${slug}`} style={{backgroundImage: `url(${photo})`}} />
     </MapMarkerWrapper>
 );
 export default MapMarker;
